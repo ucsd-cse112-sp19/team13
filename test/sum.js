80c42a7801e2923d88aa708558c4130dff3c4395
willxu1234 <wlxu@ucsd.edu>
2019-04-23 20:12:12 -0700

    add a few more tests. this commit should fail since sum has yet to be implemented

--- test/sum.js
+++ test/sum.js
@@ -7,4 +7,22 @@ describe('#sum()', () => {
       expect(sum()).to.equal(0);
     });
   });
+
+  context('with number arguments', () => {
+    it('should return the sum of arguments', () => {
+      expect(sum(1, 2, 3, 4, 5)).to.equal(15);
+    });
+
+    it('should return itself if one argument is passed', () => {
+      expect(sum(9)).to.equal(9);
+    });
+  });
+
+  context('with non-number arguments', () => {
+    it('should throw a TypeError', () => {
+      expect(() => {
+        sum(1, 2, '3', [4], 5);
+      }).to.throw(TypeError, 'sum() only expects numbers');
+    });
+  });
 });
