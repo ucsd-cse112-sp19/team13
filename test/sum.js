23204dd7cea6de544aec5b46e5d96021cbd9e8fe
willxu1234 <wlxu@ucsd.edu>
2019-04-23 19:46:06 -0700

    add eslint mocha plugin

--- test/sum.js
+++ test/sum.js
@@ -2,10 +2,9 @@ var sum = require('../sum.js');
 var expect = require('chai').expect;
 
 describe('#sum()', () => {
-context('without arguments', () => {
-it('should return 0', () => {
-expect(sum()).to.equal(0);
-})
-})
-
-})
+  context('without arguments', () => {
+    it('should return 0', () => {
+      expect(sum()).to.equal(0);
+    });
+  });
+});
