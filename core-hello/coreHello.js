fc207104190f070f1f31a47054480b8130264c4d
ank060 <ank060@ucsd.edu>
2019-04-25 16:10:12 -0700

    Fixed js linting errors

--- core-hello/coreHello.js
+++ core-hello/coreHello.js
@@ -1,6 +1,6 @@
 const coreHelloElements = document.getElementsByTagName('core-hello');
 
-export function addPrefix() {
+export default function addPrefix() {
   let i;
   for (i = 0; i < coreHelloElements.length; i += 1) {
     coreHelloElements[i].textContent = `Hello World ${coreHelloElements[i].textContent}`;
