513f2962d089f87c350d4a0b6a288a0cc8119a96
ank060 <ank060@ucsd.edu>
2019-04-25 15:52:37 -0700

    Removed old coreHello files

--- core-hello/coreHello.js
+++
@@ -1,8 +0,0 @@
-const coreHelloElements = document.getElementsByTagName('core-hello');
-
-export function addPrefix() {
-  let i;
-  for (i = 0; i < coreHelloElements.length; i += 1) {
-    coreHelloElements[i].textContent = `Hello World ${coreHelloElements[i].textContent}`;
-  }
-}
