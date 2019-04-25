330a2d1cc8e8dcea612b02bb661ed6ba1e6027d1
ank060 <ank060@ucsd.edu>
2019-04-25 15:52:04 -0700

    Fixed tab spacing and other lint errors

--- core-hello/core-hello.js
+++ core-hello/core-hello.js
@@ -1,106 +1,100 @@
-import { attachShadowRoot, createElement } from './wcutil.js';
+import { attachShadowRoot, createElement } from './wcutil';
 
-const HELLO_STRING = "Hello World";
+const HELLO_STRING = 'Hello World';
 
 /**
  * An element that displays "Hello" with the passed-in name.
  */
-export default class CoreHelloElement extends HTMLElement
-{
-    /**
-     * Creates a CoreHello element and attaches the shadow root
-     * @constructor
-     */
-    constructor()
-    {
-		super();
+export default class CoreHelloElement extends HTMLElement {
+  /**
+   * Creates a CoreHello element and attaches the shadow root
+   * @constructor
+   */
+  constructor() {
+    super();
 
-		const template = createElement('template', {}, `
+    const template = createElement('template', {}, `
             <label>
                 ${HELLO_STRING} <slot name="name"></slot>
             </label>
-			<style>
-				:host {
-					display: block;
-					contain: content;
-				}
-			</style>
-		`);
-		attachShadowRoot(this, template);
+            <style>
+                :host {
+                    display: block;
+                    contain: content;
+                }
+            </style>
+        `);
+    attachShadowRoot(this, template);
+  }
+
+  /**
+   * @override
+   */
+  static get observedAttributes() { return []; }
+
+  /**
+   * Get the attribute that represents the name that will be displayed.
+   *
+   * @type {String}
+   */
+  get name() { return this.getAttribute('name'); }
+
+  /**
+   * Set the attribute that represents the name that will be displayed.
+   *
+   * @type {String}
+   */
+  set name(opts) { this.setAttribute('name', opts); }
+
+  /**
+   * Get the attribute that represents whether the name should be colorfully animated.
+   *
+   * @type {String}
+   */
+  get rainbow() { return this.hasAttribute('rainbow'); }
+
+  /**
+   * Set the attribute that represents whether the name should be colorfully animated.
+   *
+   * @type {String}
+   */
+  set rainbow(opts) {
+    if (opts) {
+      this.setAttribute('rainbow', '');
+    } else {
+      this.removeAttribute('rainbow');
     }
+  }
 
-    /**
-     * @override
-     */
-	static get observedAttributes() { return []; }
+  /**
+   * Get the attribute that represents the language to display in.
+   *
+   * @type {String}
+   */
+  get lang() { return this.getAttribute('lang'); }
 
-    /**
-     * Get the attribute that represents the name that will be displayed.
-     * 
-     * @type {String}
-     */
-    get name() { return this.getAttribute('name'); }
+  /**
+   * Set the attribute that represents the language to display in.
+   *
+   * @type {String}
+   */
+  set lang(opts) { this.setAttribute('lang', opts); }
 
-    /**
-     * Set the attribute that represents the name that will be displayed.
-     * 
-     * @type {String}
-     */
-    set name(opts) { this.setAttribute('name', opts); }
+  /**
+   * @override
+   */
+  // connectedCallback() { }
 
-    /**
-     * Get the attribute that represents whether the name should be colorfully animated.
-     * 
-     * @type {String}
-     */
-    get rainbow() { return this.hasAttribute('rainbow'); }
+  /**
+    * @override
+    */
+  // disconnectedCallback() { }
 
-    /**
-     * Set the attribute that represents whether the name should be colorfully animated.
-     * 
-     * @type {String}
-     */
-    set rainbow(opts)
-    {
-        if (opts)
-        {
-            this.setAttribute('rainbow', '');
-        }
-        else
-        {
-            this.removeAttribute('rainbow');
-        }
-    }
-
-    /**
-     * Get the attribute that represents the language to display in.
-     * 
-     * @type {String}
-     */
-    get lang() { return this.getAttribute('lang'); }
-
-    /**
-     * Set the attribute that represents the language to display in.
-     * 
-     * @type {String}
-     */
-    set lang(opts) { this.setAttribute('lang', opts); }
-
-    /**
-     * @override
-     */
-    connectedCallback() {}
-
-    /**
-     * @override
-     */
-    disconnectedCallback() {}
-
-    /**
-     * @override
-     */
-    attributeChangedCallback(attribute, oldValue, newValue) {}
+  /**
+    * @override
+    */
+  // attributeChangedCallback(attribute, oldValue, newValue) { }
 }
 
-//Define custom element for html
+// Define custom element for html
 window.customElements.define('core-hello', CoreHelloElement);
