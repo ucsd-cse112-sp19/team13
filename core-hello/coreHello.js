144bd976151b649eae35ea2a321e6ea1067d2b8c
willxu1234 <wlxu@ucsd.edu>
2019-04-25 17:08:53 -0700

    Revert "Fixed js linting errors" because caused core-hello to not be linked
    This reverts commit fc207104190f070f1f31a47054480b8130264c4d.

--- core-hello/coreHello.js
+++ core-hello/coreHello.js
@@ -1,6 +1,6 @@
 const coreHelloElements = document.getElementsByTagName('core-hello');
 
-export default function addPrefix() {
+export function addPrefix() {
   let i;
   for (i = 0; i < coreHelloElements.length; i += 1) {
     coreHelloElements[i].textContent = `Hello World ${coreHelloElements[i].textContent}`;
