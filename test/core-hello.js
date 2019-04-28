fa018b22ee30178d2c77fe9f816de7cb36dd2059
Madeline Grubbs <mgrubbs@ucsd.edu>
2019-04-27 18:27:21 -0700

    Enable unit tests to run in browser and adds 2 unit tests for core-hello

--- test/core-hello.js
+++ test/core-hello.js
@@ -1,14 +1,39 @@
-describe('core-hello', () => {
+// view result.html for more context
+describe('p element', () => {
+  // set up test environment to have only one empty testElement in testArea
+  beforeEach('init testing environment', () => {
+    // remove testArea and reinsert before the test results for a clean slate
+    document.body.removeChild(document.getElementById('testArea'));
+    const testArea = document.createElement('div');
+    testArea.setAttribute('id', 'testArea');
+    document.body.insertBefore(testArea, document.getElementById('testTitle'));
+    // create the test element
+    const testElement = document.createElement('p');
+    // set id value
+    testElement.setAttribute('id', 'testElement');
+    // insert into DOM as child of element with id as testArea
+    testArea.appendChild(testElement);
+  });
+
+  // test 1: Check that testElement is inserted into the DOM
+  context('Inserted in DOM', () => {
+    it('should return non-null object', () => {
+      // retrieve testElement from the DOM
+      const isRetrievable = document.getElementById('testElement');
+      chai.expect(isRetrievable).to.not.equal(null);
+    });
+  });
+
   // test 2: Check whatever's in the "slot" is displayed correctly
   context('Text in Slot', () => {
     it('should be Hello World, CSE 112', () => {
-      // create the core-hello element
-      const myCoreHello = document.createElement('core-hello');
-      // the text in myCoreHello slot
+      // retrieve the test element
+      const testElement = document.getElementById('testElement');
+      // the text in testElement slot
       const message = 'Hello World, CSE 112';
       // set it's inner text
-      myCoreHello.innerHTML = message;
-      chai.expect(myCoreHello.innerHTML).to.equal(message);
+      testElement.innerHTML = message;
+      chai.expect(testElement.innerHTML).to.equal(message);
     });
   });
 });
