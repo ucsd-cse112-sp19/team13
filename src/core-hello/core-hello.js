e8b3762df6a7e1d8351275b95ab6aa57506b4b3c
ank060 <ank060@ucsd.edu>
2019-04-29 19:10:01 -0700

    Added webpack

---
+++ src/core-hello/core-hello.js
@@ -0,0 +1,107 @@
+import TEMPLATE from './core-hello.html';
+import STYLE from './core-hello.css';
+import { createTemplate, attachShadowRoot, registerCustomTag } from '../wcutil';
+
+const template = createTemplate(TEMPLATE, STYLE);
+
+const HELLO_STRING = {
+  en: 'Hello World',
+  es: 'Hola Mundo',
+  jp: 'こんにちは世界',
+  fr: 'Bonjour le monde',
+};
+
+/**
+ * An element that displays "Hello" with the passed-in name.
+ */
+class CoreHelloElement extends HTMLElement {
+  /**
+   * Creates a CoreHello element and attaches the shadow root
+   * @constructor
+   */
+  constructor() {
+    super();
+    attachShadowRoot(this, template);
+
+    this.helloElement = this.shadowRoot.querySelector('#hello');
+  }
+
+  /**
+   * @override
+   */
+  // connectedCallback() { }
+
+  /**
+    * @override
+    */
+  // disconnectedCallback() { }
+
+  /**
+   * @override
+   */
+  attributeChangedCallback(attribute, oldValue, newValue) {
+    switch (attribute) {
+      case 'lang':
+        this.helloElement.textContent = HELLO_STRING[newValue] || HELLO_STRING.en;
+        break;
+      default:
+    }
+  }
+
+  /**
+   * @override
+   */
+  static get observedAttributes() { return ['lang']; }
+
+  /**
+   * Get the attribute that represents the name that will be displayed.
+   *
+   * @type {String}
+   */
+  get name() { return this.getAttribute('name'); }
+
+  /**
+   * Set the attribute that represents the name that will be displayed.
+   *
+   * @type {String}
+   */
+  set name(opts) { this.setAttribute('name', opts); }
+
+  /**
+   * Get the attribute that represents whether the name should be colorfully animated.
+   *
+   * @type {String}
+   */
+  get rainbow() { return this.hasAttribute('rainbow'); }
+
+  /**
+   * Set the attribute that represents whether the name should be colorfully animated.
+   *
+   * @type {String}
+   */
+  set rainbow(opts) {
+    if (opts) {
+      this.setAttribute('rainbow', '');
+    } else {
+      this.removeAttribute('rainbow');
+    }
+  }
+
+  /**
+   * Get the attribute that represents the language to display in.
+   *
+   * @type {String}
+   */
+  get lang() { return this.getAttribute('lang'); }
+
+  /**
+   * Set the attribute that represents the language to display in.
+   *
+   * @type {String}
+   */
+  set lang(opts) { this.setAttribute('lang', opts); }
+}
+
+registerCustomTag('core-hello', CoreHelloElement);
+
+export default CoreHelloElement;
