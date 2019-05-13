0e3b42cd3c224e384cd5fd5e6084006063930d8c
Keven Wilbert Saputra <ksaputra@ucsd.edu>
2019-05-12 23:07:19 -0700

    delete app.js and add some comment to test file

--- test/core-hello.js
+++ test/core-hello.js
@@ -8,20 +8,38 @@ const HELLO_STRING = {
   fr: 'Bonjour le monde',
 };
 
+/**
+ * Main testing method using mocha 
+ */
 
 describe('core-hello', () => {
+  /**
+   * Starting showroom and puppeter in order to create the html document 
+   * to be tested
+   */
   before(async () => {
     await showroom.start();
   });
 
+  /**
+   * Stopping showroom and puppeter 
+   */
   after(async () => {
     await showroom.stop();
   });
 
+
+  /**
+   * Creating new core-hello component for each test cases wihtout any attribute
+   */
   beforeEach(async () => {
     await showroom.setTestSubject('core-hello');
   });
 
+
+  /**
+   * This test check whether the message is in english
+   */
   it(`Message should be ${HELLO_STRING.en}`, async () => {
     await showroom.setAttribute('lang', 'en');
     const outerHello = await showroom.find('// span');
@@ -30,6 +48,9 @@ describe('core-hello', () => {
     assert.equal(outerMessage, HELLO_STRING.en);
   });
 
+  /**
+   * This test check whether the message is in spanish
+   */
   it(`Message should be ${HELLO_STRING.es}`, async () => {
     await showroom.setAttribute('lang', 'es');
     const outerHello = await showroom.find('// span');
@@ -38,6 +59,9 @@ describe('core-hello', () => {
     assert.equal(outerMessage, HELLO_STRING.es);
   });
 
+  /**
+   * This test check whether the message is in japanese
+   */
   it(`Message should be ${HELLO_STRING.jp}`, async () => {
     await showroom.setAttribute('lang', 'jp');
     const outerHello = await showroom.find('// span');
@@ -46,6 +70,9 @@ describe('core-hello', () => {
     assert.equal(outerMessage, HELLO_STRING.jp);
   });
 
+  /**
+   * This test check whether the message is in france
+   */
   it(`Message should be ${HELLO_STRING.fr}`, async () => {
     await showroom.setAttribute('lang', 'fr');
     const outerHello = await showroom.find('// span');
@@ -54,6 +81,9 @@ describe('core-hello', () => {
     assert.equal(outerMessage, HELLO_STRING.fr);
   });
 
+  /**
+   * This test check whether the rainbow component exist or not
+   */
   it('Rainbow attribute should exist and working', async () => {
     await showroom.setAttribute('rainbow');
     const attrExist = await showroom.hasAttribute('rainbow');
