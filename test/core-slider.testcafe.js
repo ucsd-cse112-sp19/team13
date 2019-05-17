b5e8fb4eabd8d7457d75b2429ff1eaaba70ce426
Keven Wilbert Saputra <ksaputra@ucsd.edu>
2019-05-16 22:34:25 -0700

    porting test from showroom to testcafe

--- test/core-slider.testcafe.js
+++ test/core-slider.testcafe.js
@@ -1,19 +1,92 @@
 import { Selector } from 'testcafe';
 
+// Slider component
 const slider = Selector('#size-slider');
-const sliderThumb = Selector(() => document.querySelector('#size-slider').shadowRoot.querySelector('#slider-thumb'))
+const disabledSlider = Selector('#disabled-slider');
+const rainbowSlider = Selector('#target-slider-rainbow');
 
+// Slider's thumb component
+const sliderThumb = Selector(() => document.querySelector('#size-slider').shadowRoot.querySelector('#slider-thumb'));
+
+const disabledThumb = Selector(() => document.querySelector('#disabled-slider').shadowRoot.querySelector('#slider-thumb'));
+
+const targetThumbArb = Selector(() => document.querySelector('#target-slider-arbitrary').shadowRoot.querySelector('#slider-thumb'));
+
+const targetThumbMax = Selector(() => document.querySelector('#target-slider-max').shadowRoot.querySelector('#slider-thumb'));
+
+const targetThumbMin = Selector(() => document.querySelector('#target-slider-min').shadowRoot.querySelector('#slider-thumb'));
+
+// fixture, getting the page for testing
 fixture `Core Slider Test`
     .page `../dist/index.html`;
 
-/** 
+/**
+ * Tests for default value of core-slider
+ */
+test('Default core-slider', async (t) => {
+  await t
+    .expect(slider.value).eql('60');
+});
+
+/**
+ * Tests for initialized value of core-slider
+ *
+ * NOTE: This needs to be fixed
+ */
+test('Initialized core-slider', async (t) => {
+  await t
+    .expect(slider.value).eql('60');
+});
+
+/**
  * Tests for value Attribute of Core-Slider
  */
-test('Dragging core-slider', async t => {
-    await t
-        .setTestSpeed(0.5)
-        .click(sliderThumb)
-        .expect(slider.value).eql('50')
-        .drag(sliderThumb, 30, 0)
-        .expect(slider.value).eql('70');
+test('Dragging core-slider for value check', async (t) => {
+  await t
+    .dragToElement(sliderThumb, disabledThumb)
+    .expect(slider.value).eql('50');
+});
+
+/**
+ * Tests for steps Attribute of Core-Slider
+ */
+test('Dragging core-slider for steps check', async (t) => {
+  await t
+    .dragToElement(sliderThumb, targetThumbArb)
+    .expect(slider.value).eql('30');
+});
+
+/**
+ * Tests for min attribute of core-slider
+ */
+test('Min attribute', async (t) => {
+  await t
+    .dragToElement(sliderThumb, targetThumbMin)
+    .expect(slider.value).eql('10');
+});
+
+/**
+ * Tests for max attribute of core-slider
+ */
+test('Max attribute', async (t) => {
+  await t
+    .dragToElement(sliderThumb, targetThumbMax)
+    .expect(slider.value).eql('100');
+});
+
+/**
+ * Tests for disabled attribute of core-slider
+ */
+test('Disabled attribute', async (t) => {
+  await t
+    .dragToElement(disabledThumb, targetThumbArb)
+    .expect(disabledSlider.value).eql('50');
+});
+
+/**
+ * Tests for rainbow attribute existence of core-slider
+ */
+test('Rainbow attribute', async (t) => {
+  await t
+    .expect(rainbowSlider.hasAttribute('rainbow')).ok();
 });
