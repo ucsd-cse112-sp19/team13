d563c988289be4d4cc033407f73bb78959693507
KevenSaputra <ksaputra@ucsd.edu>
2019-05-30 14:54:36 -0700

    add template for light and dark theme

--- test/core-tooltip.testcafe.js
+++ test/core-tooltip.testcafe.js
@@ -17,3 +17,23 @@ testCoreTooltip('content', '- Content value', async (t, ctx) => {
   await t
     .expect(0).eql(0);
 });
+
+/** Tests for light theme of core-tooltip */
+testCoreTooltip('effect', '- light effect', async (t, ctx) => {
+  const tooltip = ctx.target;
+  const tooltipBox = ShadowChildSelector(t, ctx, '#tooltip-child-name-temp')
+  await t
+    .hover(tooltip)
+    .expect(tooltipBox.getStyleProperty('_________'))
+    .eql('_____');
+})
+
+/** Tests for dark theme of core-tooltip */
+testCoreTooltip('effect', '- dark effect', async (t) => {
+  const tooltip = ctx.target;
+  const tooltipBox = ShadowChildSelector(t, ctx, '#tooltip-child-name-temp')
+  await t
+    .hover(tooltip)
+    .expect(tooltipBox.getStyleProperty('_________'))
+    .eql('_____');
+})
