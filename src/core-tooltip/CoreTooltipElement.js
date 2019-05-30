5ea2b089e6e8e3de697cb5eba49a601cb3c23348
ank060 <ank060@ucsd.edu>
2019-05-29 20:53:20 -0700

    Compile dist and docs

--- src/core-tooltip/CoreTooltipElement.js
+++ src/core-tooltip/CoreTooltipElement.js
@@ -1,4 +1,4 @@
-import CoreElement from '../CoreElement';
+import CoreElement from '../core-element/CoreElement';
 
 import TEMPLATE from './CoreTooltipElement.html';
 import STYLE from './CoreTooltipElement.css';
