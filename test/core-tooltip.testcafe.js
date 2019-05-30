58bc56fc1defe1b901b9275814c0773e4b65516a
unknown <mgrubbs@ucsd.edu>
2019-05-30 16:20:38 -0700

    adds unit tests for content attribute of tooltip

--- test/core-tooltip.testcafe.js
+++ test/core-tooltip.testcafe.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import { ClientFunction } from 'testcafe';
 import { TestCoreElement, ShadowChildSelector } from './testcore';
 
 /* eslint-disable */
@@ -12,8 +13,39 @@ function testCoreTooltip(sectionName, testDesc, testFunc) {
 }
 
 /** Tests for content attribute of core-tooltip */
-testCoreTooltip('content', '- Content value', async (t, ctx) => {
-  const tooltip = ctx.target;
+testCoreTooltip('content', '- Exists an attribute named content', async (t, ctx) => {
+  const getExistence = ClientFunction(() => {
+    const testElement = document.getElementById('content-exists');
+    // if content is among tooltip's listed attributes, then return true
+    if((testElement.attributes).content){
+      return true;
+    } else {
+      return false;
+    }
+  });
+  const existsValue = await getExistence();
   await t
-    .expect(0).eql(0);
+    .expect(existsValue).eql(true);
+});
+
+testCoreTooltip('content', '- Default content value should be empty string', async (t, ctx) => {
+  const getContent = ClientFunction(() => {
+    const testElement = document.getElementById('content-default');
+    return testElement.content;
+  });
+  const contentValue = await getContent();
+  await t
+    .expect(contentValue).eql('');
+});
+
+testCoreTooltip('content', '- Set content value should be Team Friday Tooltip', async (t, ctx) => {
+  const message = 'Team Friday Tooltip';
+  const getContent = ClientFunction((strMsg) => {
+    const testElement = document.getElementById('content-set');
+    testElement.content = strMsg;
+    return testElement.content;
+  });
+  const contentValue = await getContent(message);
+  await t
+    .expect(contentValue).eql(message);
 });
