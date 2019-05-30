d36b61790ebca6815ceafbe9599924303721fb0f
unknown <mgrubbs@ucsd.edu>
2019-05-30 10:54:30 -0700

    adds the testing file for core-tooltip

---
+++ test/core-tooltip.testcafe.js
@@ -0,0 +1,17 @@
+import { TestCoreElement, ShadowChildSelector } from './testcore';
+
+/* eslint-disable */
+fixture `core-tooltip tests`
+    .page `./core-tooltip-index.html`;
+/* eslint-enable */
+
+function testCoreTooltip(sectionName, testDesc, testFunc) {
+  TestCoreElement('core-tooltip', sectionName, testDesc, testFunc);
+}
+
+/** Tests for content attribute of core-tooltip */
+testCoreTooltip('content', '- Content value', async (t, ctx) => {
+  const tooltip = ctx.target;
+  await t
+    .expect(0).eql(0);
+});
