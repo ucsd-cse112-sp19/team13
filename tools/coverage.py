7bbe5716671140a3419c9b8334e6d69516e51c77
willxu1234 <wlxu@ucsd.edu>
2019-05-29 18:53:14 -0700

    add python script

--- tools/coverage.py
+++ tools/coverage.py
@@ -1,2 +1,42 @@
-if 1:
-    print('hello world')
+import json
+import os
+
+def main():
+    with open('../coverage/coverage-final.json') as infile:
+        # Import coverage report as a dictionary.
+        coverage = json.load(infile)
+        for file_name in coverage:
+            # Completely remove statements and branches.
+            coverage[file_name]["s"].clear()
+            coverage[file_name]["b"].clear()
+            # For functions, remove if we can find this function
+            # being called from a test file.
+            keys = list(coverage[file_name]["f"])
+            for fn_num in keys:
+                fn_name = coverage[file_name]["fnMap"][fn_num]["name"]
+                if not search_in_test(fn_name):
+                    del coverage[file_name]["f"][fn_num] 
+
+    # Stick the json back into the original file. 
+    with open('../coverage/coverage-py.json', 'w') as outfile:
+        json.dump(coverage, outfile)
+
+# Used for finding a string (or in our case, a function name) in test dir.
+def search_in_test(fn_name):
+    # Get each file name in ../test
+    for file_name in os.listdir(path='../test'):
+        # Open that file.
+        f = open('../test/' + file_name)
+        cur_line = f.readline()
+
+        # See if we can find the string in the line.
+        while cur_line:
+            if cur_line.find(fn_name) != -1:
+                print(f'found {fn_name} in {file_name}')
+                return True
+            cur_line = f.readline()
+
+    return False
+
+if __name__ == '__main__':
+    main()
