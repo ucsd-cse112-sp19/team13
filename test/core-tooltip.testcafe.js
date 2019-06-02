ff7feab27613a47d7458ce10fdcd1514942a5250
unknown <mgrubbs@ucsd.edu>
2019-06-02 14:22:46 -0700

    adds tests for disabled attribute of core tooltip

--- test/core-tooltip.testcafe.js
+++ test/core-tooltip.testcafe.js
@@ -12,41 +12,63 @@ function testCoreTooltip(sectionName, testDesc, testFunc) {
   TestCoreElement('core-tooltip', sectionName, testDesc, testFunc);
 }
 
-/** Tests for content attribute of core-tooltip */
-testCoreTooltip('content', '- Exists an attribute named content', async (t, ctx) => {
-  const getExistence = ClientFunction(() => {
-    const testElement = document.getElementById('content-exists');
-    // if content is among tooltip's listed attributes, then return true
-    if ((testElement.attributes).content) {
-      return true;
-    }
-    return false;
-  });
-  const existsValue = await getExistence();
-  await t
-    .expect(existsValue).eql(true);
+/**
+ * Purpose: calls and returns the value of get strAttrName of the tooltip with id strElemId
+ *
+ * @param {String} strAttrName the name of the attribute to get
+ * @param {String} strElemId the id of the tested element.
+ */
+const getCoreAttribute = ClientFunction((strAttrName, strElemId) => {
+  const testElement = document.getElementById(strElemId);
+  const getExpression = 'testElement.' + strAttrName;
+  return eval(getExpression);
 });
 
+/**
+ * Purpose: calls set strAttrName of the tooltip with id strElemId
+ * Note: the return value is an empty string and has no meaning
+ *
+ * @param {String} strAttrName the name of the attribute to get
+ * @param {String} strElemId the id of the tested element.
+ * @param value the value strAttrName should be set to. For strings,they need to be in format '"example string"'
+ */
+const setCoreAttribute = ClientFunction((strAttrName, strElemId, value) => {
+  const testElement = document.getElementById(strElemId);
+  // set the strAttrName to value
+  const setExpression = 'testElement.' + strAttrName + ' = ' + value;
+  eval(setExpression);
+  return '';
+});
+
+/** Tests for content attribute of core-tooltip */
 testCoreTooltip('content', '- Default content value should be empty string', async (t, ctx) => {
-  const getContent = ClientFunction(() => {
-    const testElement = document.getElementById('content-default');
-    return testElement.content;
-  });
-  const contentValue = await getContent();
+  const defaultValue = await getCoreAttribute('content', 'content-default');
   await t
-    .expect(contentValue).eql('');
+    .expect(defaultValue).eql('');
 });
 
 testCoreTooltip('content', '- Set content value should be Team Friday Tooltip', async (t, ctx) => {
-  const message = 'Team Friday Tooltip';
-  const getContent = ClientFunction((strMsg) => {
-    const testElement = document.getElementById('content-set');
-    testElement.content = strMsg;
-    return testElement.content;
-  });
-  const contentValue = await getContent(message);
+  const setMessage = '"Team Friday Tooltip"'; // message to set content
+  const evalMessage = 'Team Friday Tooltip'; // message the set content should evaluate to
+  await setCoreAttribute('content', 'content-set', setMessage);
+  const setValue = await getCoreAttribute('content', 'content-set');
   await t
-    .expect(contentValue).eql(message);
+    .expect(setValue).eql(evalMessage);
+});
+
+/** Tests for disable attribute of core-tooltip */
+testCoreTooltip('disabled', '- Default disabled value should be false', async (t, ctx) => {
+  const defaultValue = await getCoreAttribute('disabled', 'disabled-default');
+  await t
+    .expect(defaultValue).eql(false);
+});
+
+testCoreTooltip('disabled', '- Set disabled value should be true', async (t, ctx) => {
+  const isDisabled = true;
+  await setCoreAttribute('disabled', 'disabled-set', isDisabled);
+  const setValue = await getCoreAttribute('disabled', 'disabled-set');
+  await t
+    .expect(setValue).eql(isDisabled);
 });
 
 /** Tests for light theme of core-tooltip */
