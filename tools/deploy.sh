c26da62788e20e4377bb63b65122e9b695f1ce4e
travis-ci <travis@travis-ci.org>
2019-06-01 18:30:49 -0700

    test

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -9,14 +9,15 @@
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ucsd-cse112/team13 gh-pages > /dev/null
+  git clone --quiet --branch=jsdocs https://${GH_TOKEN}@github.com/ucsd-cse112/team13 jsdocs > /dev/null
 
-  cd team13/docs
+  cd docs
+
   git rm -rf ./javadoc
   cp -Rf $HOME/javadoc-latest ./javadoc
   git add -f .
   git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin jsdocs > /dev/null
 
   echo -e "Published Javadoc to gh-pages.\n"
   
