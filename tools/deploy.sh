380a4f30199e597ec5669f15f1c631725c110a6d
travis-ci <travis@travis-ci.org>
2019-06-01 18:21:21 -0700

    should work now

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,21 +2,22 @@
 
 # if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
-	echo -e "Publishing javadoc...\n"
-	cd $HOME
-	git config --global user.email "travis@travis-ci.org"
-	git config --global user.name "travis-ci"
-	git clone --quiet https://${GH_TOKEN}@github.com/ucsd-cse112/team13
+  echo -e "Publishing javadoc...\n"
 
-	npm run docs
+  cp -R docs/ $HOME/javadoc-latest
 
-	# Commit and Push the Changes
-	cd ./docs
-	# git rm -rf ./javadoc
-	# cp -Rf $HOME/javadoc-latest ./javadoc
-	git add -f .
-	git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to docs"
-	git push -fq origin docs > /dev/null
-	echo -e "Published Javadoc to /docs.\n"
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ucsd-cse112/team13 gh-pages > /dev/null
+
+  cd team13/docs
+  git rm -rf ./javadoc
+  cp -Rf $HOME/javadoc-latest ./javadoc
+  git add -f .
+  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published Javadoc to gh-pages.\n"
   
 # fi
