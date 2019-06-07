a9948966ebb0333296f7ee5711781773f67d8a5b
ank060 <ank060@ucsd.edu>
2019-06-06 19:44:49 -0700

    Add babel for transpiling to ES5

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,11 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
 				test: /\.html$/,
 				use: [
 					{
