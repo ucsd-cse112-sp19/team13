cf109b74800f9e5ae82de7ff69adaa99a6bd943c
sssaiya <sssaiya@ucsd.edu>
2019-06-06 21:17:19 -0700

    Add config file for storybook

---
+++ .storybook/config.js
@@ -0,0 +1,10 @@
+import { configure } from '@storybook/html';
+import 'core-js';
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../stories', true, /\.stories\.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
