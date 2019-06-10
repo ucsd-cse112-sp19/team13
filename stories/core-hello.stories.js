8a42a53b16f631728cdcf3ffe69556dd87531d9d
sssaiya <sssaiya@ucsd.edu>
2019-06-09 23:56:41 -0700

    Fix core-hello stories

--- stories/core-hello.stories.js
+++ stories/core-hello.stories.js
@@ -1,10 +1,32 @@
 import { storiesOf } from '@storybook/html';
+import { boolean } from '@storybook/addon-knobs/polymer';
 import '../src/core-greet/core-hello/CoreHelloElement';
 import '../src/core-greet/CoreGreetElement';
 
 storiesOf('Core Hello', module)
-  .add('English', () => '<core-hello lang="en">PETER</core-hello>')
-  .add('Rainbow', () => '<core-hello lang="en" rainbow>PETER</core-hello>')
-  .add('Spanish', () => '<core-hello lang="es">PETER</core-hello>')
-  .add('Japanese', () => '<core-hello lang="js">PETER</core-hello>')
-  .add('French', () => '<core-hello lang="fr">PETER</core-hello>');
+  .add('English', () => '<core-hello lang="en">Peter</core-hello>')
+  .add('Rainbow', () => {
+    const coreHello0 = document.createElement('core-hello');
+    coreHello0.innerText = 'Peter';
+    coreHello0.lang = 'en';
+    coreHello0.rainbow = boolean('Rainbow', false);
+    return coreHello0;
+  })
+  .add('Spanish', () => {
+    const coreHello1 = document.createElement('core-hello');
+    coreHello1.innerText = 'Peter';
+    coreHello1.lang = 'es';
+    return coreHello1;
+  })
+  .add('French', () => {
+    const coreHello2 = document.createElement('core-hello');
+    coreHello2.innerText = 'Peter';
+    coreHello2.lang = 'fr';
+    return coreHello2;
+  })
+  .add('Japanese', () => {
+    const coreHello3 = document.createElement('core-hello');
+    coreHello3.innerText = 'Peter';
+    coreHello3.lang = 'jp';
+    return coreHello3;
+  });
