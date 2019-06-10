171ccf83ca34ca4ad21657f0a45b95539ba00809
sssaiya <sssaiya@ucsd.edu>
2019-06-09 23:56:58 -0700

    add core-slider story
    THEY STILL DONT WORK :(

---
+++ stories/core-slider.stories.js
@@ -0,0 +1,20 @@
+import { storiesOf } from '@storybook/html';
+// import { withKnobs } from '@storybook/addon-knobs/polymer';
+// import { action } from '@storybook/addon-actions';
+import coreslider from '../src/core-slider/CoreSliderElement';
+// import '../src/core-slider/CoreSliderElement.html';
+// import '../src/core-slider/CoreSliderElement.css';
+
+storiesOf('Core Slider', module)
+  .add('English', () => '<core-slider>Peter</core-slider>')
+  // .addDecorator(withKnobs)
+  .add('Default', () => {
+    const slider = document.createElement('core-slider');
+    slider.type = 'core-slider';
+    slider.innerText = 'demo';
+    return slider;
+  })
+  .add('as a component', () => ({
+    components: { coreslider },
+    template: '<core-slider></core-slider>',
+  }));
